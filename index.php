d96bc2896328c7d85f39f6fe67819694d4ee2c64
JessieVela <33134794+JessieVela@users.noreply.github.com>
2017-11-01 21:15:27 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>
-HomePage
-</title>    
+    <title>
+        HomePage
+    </title>
     <link href="https://fonts.googleapis.com/css?family=Kumar+One" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="homepage.css" type="text/css">
@@ -11,71 +11,61 @@ HomePage
     <script src="homepage.js"></script>
 </head>
 <body>
-   
 
-     <nav class="navbar navbar-inverse">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">The Atomic Cat</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-      <li class="active"><a href="homepage.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-      
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Sign Up <i class="fa fa-user-plus" aria-hidden="true"></i>
-</a></li>
-        <li><a href="#">Login <i class="fa fa-user" aria-hidden="true"></i>
-</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">The Atomic Cat</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Sign Up <i class="fa fa-user-plus" aria-hidden="true"></i>
+                    </a></li>
+                <li><a href="#">Login <i class="fa fa-user" aria-hidden="true"></i>
+                    </a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
-    
+
 <div class="container">
     <div class="row">
-    <div class="col-lg-12">
-        <div id="content">
-         <h1>Hello Welcome </h1>
-        <hr>
-        <button class="btn btn-default bt-lg"><i class="fa fa-user" aria-hidden="true"></i> Get Started!</button>
-        </div>
+        <div class="col-lg-12">
+            <div id="content">
+                <h1>Hello Welcome </h1>
+                <form action = "upload.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+                        application/vnd.ms-excel"><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
         </div>
     </div>
-    </div>
-    
-<!--
-    <div class="container2">
-    <div class="row">
-    <div class="col-lg-12">
-        <div id="content2">
-         <p>@copyright Alio'k</p>
-    
-        </div>
-        </div>
-        </div>
-    
-    </div>
--->
-    
- <script
-  src="https://code.jquery.com/jquery-3.1.1.js"
-  integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
-  crossorigin="anonymous"></script>
-        
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>         
+</div>
+
+<!-- what is this script below for -->
+<script
+        src="https://code.jquery.com/jquery-3.1.1.js"
+        integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+        crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 
 </html>
